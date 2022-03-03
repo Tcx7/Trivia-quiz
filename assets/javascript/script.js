15973e3fc1ea1d7f126c89e82329e6509ac4f038
Tcx7 <98015500+Tcx7@users.noreply.github.com>
2022-03-03 01:03:12 -0500

    randomized orders for questions and answers

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,32 +9,47 @@ var randomFunction = function (obj) {
   var keys = Object.keys(obj);
   return obj[keys[(keys.length * Math.random()) << 0]];
 };
+function shuffle(a) {
+  var j, x, i;
+  for (i = a.length - 1; i > 0; i--) {
+    j = Math.floor(Math.random() * (i + 1));
+    x = a[i];
+    a[i] = a[j];
+    a[j] = x;
+  }
+  return a;
+}
 
 var Question_Ans = {
   question1: {
     question: "Animals that only eat plants are called:",
     choices: ["Plantivores", "Herbivores", "Vegevores", "Fernavores"],
     correct: 0,
+    correctAns: "Herbivores",
   },
   question2: {
     question: "What's the nickname of the Academy Awards?",
     choices: ["The Tonys", "The Oscars", "The Nemos", "The Rogers"],
     correct: 0,
+    correctAns: "The Oscars",
   },
   question3: {
     question: "Where did the Olympics originate?",
     choices: ["France", "Greece", "China", "Egypt"],
     correct: 0,
+    correctAns: "Greece",
   },
   question4: {
     question: "The Egyptian Sphinx’s body is based on which animal?",
     choices: ["Hyena", "Lion", "Goat", "Horse"],
     correct: 0,
+    correctAns: "Lion",
   },
   question5: {
     question: "Vampires are typically known for turning into which animal?",
     choices: ["Moose", "Bats", "Monkeys", "Cows"],
     correct: 0,
+    correctAns: "Bats",
   },
 };
 var size = Object.keys(Question_Ans).length;
@@ -49,7 +64,8 @@ function createQuiz() {
   if (randomQuestionObj.correct === 1) {
     createQuiz();
   } else if (randomQuestionObj.correct === 0) {
-    let randomChoices = randomQuestionObj.choices;
+    let randomChoices = shuffle(randomQuestionObj.choices);
+    console.log(randomChoices);
     question.innerHTML = `${randomQuestionObj.question}`;
     for (let i = 0; i < 4; i++) {
       let element = document.createElement("li");
@@ -63,7 +79,9 @@ function createQuiz() {
       element.appendChild(createBtn);
       htmlElem.appendChild(element);
       document.getElementById(`pickBtn${i}`).addEventListener("click", (e) => {
-        if (e.target.parentNode.innerHTML.includes(randomChoices[1])) {
+        if (
+          e.target.parentNode.innerHTML.includes(randomQuestionObj.correctAns)
+        ) {
           alert("Correct answer!");
           randomQuestionObj.correct = 1;
           correctNumbers++;
